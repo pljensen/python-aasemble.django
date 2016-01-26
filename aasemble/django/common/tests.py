6e87e18a454007493f10eafc256c02f6f8b20844
Peter Jensen <pljensen12345@gmail.com>
2016-01-26 16:24:38 +0000

    Add a way to selectively grant users access to features

---
+++ aasemble/django/common/tests.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+
+from aasemble.django.common import models
+from aasemble.django.common.utils import user_has_feature
+from aasemble.django.tests import AasembleTestCase as TestCase
+
+class AasembleUserTestCase(TestCase):
+    def test_has_feature_that_is_on_by_default(self):
+        user = User.objects.get(id=1)
+        self.assertTrue(user_has_feature(user, 'DUMMY_FEATURE'))
+
+    def test_does_not_have_feature_that_is_off_by_default(self):
+        user = User.objects.get(id=1)
+        self.assertFalse(user_has_feature(user, 'DUMMY_FEATURE_2'))
+
+    def test_user_has_feature_when_given(self):
+        user = User.objects.get(id=1)
+        feature = models.Feature.objects.get(name='DUMMY_FEATURE_2')
+        feature.users.add(user)
+        self.assertTrue(user_has_feature(user, 'DUMMY_FEATURE_2'))
