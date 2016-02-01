bad46d7b5d5250d0bbdcb710d4b36e8857934f93
Peter Jensen <pljensen12345@gmail.com>
2016-02-01 21:59:46 +0000

    Add tests for import_dsc and import_deb

--- aasemble/django/apps/buildsvc/models/binary_package_version_user_field.py
+++ aasemble/django/apps/buildsvc/models/binary_package_version_user_field.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class BinaryPackageVersionUserField(models.Model):
     binary_package_version = models.ForeignKey('BinaryPackageVersion')
     name = models.CharField(max_length=100)
